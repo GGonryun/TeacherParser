3e899bdd1e8c5c1f79e17b7a05642e1bdd9dcb74
Miguel Campos <amodestduck@gmail.com>
2019-03-14 23:11:07 -0700

    Adding HTML Fetcher
    Feature.
    We are now able to scrape source html off a document.
    -

--- ClassStatistics/HTMLFetcher.cs
+++ ClassStatistics/HTMLFetcher.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,14 @@ namespace ClassStatistics
 {
     public class HTMLFetcher
     {
+        public string BaseUri { get; private set; }
+        public string Arguments { get; private set; }
+        public HtmlDocument Content { get; private set; }
 
+        public HTMLFetcher(string uri, string arguments)
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load(uri + arguments);
+        }
     }
 }
