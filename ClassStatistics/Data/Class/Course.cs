cb7d5dad76ed86302db6ac05ab92055a19f94d03
Miguel Campos <amodestduck@gmail.com>
2019-03-15 19:43:51 -0700

    Adding Course
    Feature.
    Allows us to compare course difficulty.
    -

---
+++ ClassStatistics/Data/Class/Course.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class
+{
+    public class Course
+    {
+        public string Subject { get; private set; }
+        public int Number { get; private set; }
+
+        public Course(string subject, int number)
+        {
+            this.Subject = subject;
+            this.Number = number;
+        }
+
+        public override string ToString()
+        {
+            return $"{Subject}-{Number}":
+        }
+    }
+}
