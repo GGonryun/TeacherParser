40339271855caa07cfe4803a367baebd07b260e7
Miguel Campos <amodestduck@gmail.com>
2019-03-15 18:54:39 -0700

    Adding Parser Tests
    Chore.
    We feel safer because our modules test fine :)
    -

--- ClassStatistics/HTML/Parser/ParserTests.cs
+++ ClassStatistics/HTML/Parser/ParserTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using NUnit.Framework;
 
@@ -8,12 +9,51 @@ namespace ClassStatistics
     class ParserTests
     {
         Fetcher fetcher;
-        Parser parser;
+        Parser elementParser;
+        string elementPattern;
+        Parser classParser;
+        string classPattern;
+        string startUpPath;
 
-        [SetUp] public void SetUp()
+        [SetUp]
+        public void SetUp()
         {
+            classPattern = "//*[contains(concat(' ', @class, ' '), ' apple ')]";
+            elementPattern = "//b";
+            startUpPath = Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.FullName, "HTML", "RawHTML.txt");
+            fetcher = new Fetcher(Source.Local, startUpPath);
+            elementParser = new Parser(fetcher.Document, elementPattern);
+            classParser = new Parser(fetcher.Document, classPattern);
         }
 
-        
+        [Test]
+        public void ClassParserValidArguments()
+        {
+            Assert.AreEqual(fetcher.Document, classParser.Document);
+            Assert.AreEqual(classPattern, classParser.Pattern);
+        }
+
+        [Test]
+        public void ClassParserFindsClass()
+        {
+            Assert.IsNotNull(classParser.Nodes);
+            Assert.AreEqual(1, classParser.Nodes.Count);
+        }
+
+        [Test]
+        public void ElementParserValidArguments()
+        {
+            Assert.AreEqual(fetcher.Document, elementParser.Document);
+            Assert.AreEqual(elementPattern, elementParser.Pattern);
+        }
+
+        [Test]
+        public void ElementParserFindsElement()
+        {
+            Assert.IsNotNull(elementParser.Nodes);
+            Assert.AreEqual(1, elementParser.Nodes.Count);
+        }
+
+
     }
 }
